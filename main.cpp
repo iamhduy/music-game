2dda42aa8d3d46aa77f51d53e4f191ddd0db2d08
Milo <baranmil@msu.edu>
2024-02-20 14:40:08 -0500

    If we had scoreboard class, maybe something like this could work to keep score.

--- main.cpp
+++ main.cpp
@@ -5,3 +5,18 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+//If we had scoreboard class, could do something like
+//this to keep score
+//void ScoreBoard::KeepScore()
+//{
+//    if (SoundPlayedSuccessfully() && NotLongSound())
+//    {
+//        mScore += 10;
+//    }
+//    else if (SoundPlayedSuccessfully() && LongSound())
+//    {
+//        mScore += 10 * DurationPlayed();
+//    }
+//
+//}
