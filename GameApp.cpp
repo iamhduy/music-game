9f0231765a4bf7cbe9cdbcae9df9af26d7869f77
ruangene <ruangene@msu.edu>
2024-02-22 17:14:51 -0500

    Replace GameApp.cpp

--- GameApp.cpp
+++ GameApp.cpp
@@ -6,6 +6,12 @@
 #include "GameApp.h"
 #include <MainFrame.h>
 
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
+/// The audio engine for miniaudio
+ma_engine mAudioEngine;
+
 bool GameApp::OnInit()
 {
     if (!wxApp::OnInit())
@@ -13,6 +19,22 @@ bool GameApp::OnInit()
     auto frame = new MainFrame();
     frame->Initialize();
     frame->Show(true);
+
+    // Initialize the audio engine
+    auto result = ma_engine_init(NULL, &mAudioEngine);
+    if (result != MA_SUCCESS)
+    {
+        wxString msg;
+        msg.Printf(L"Unable to initialize miniaudio engine - %d", result);
+        wxMessageBox( msg, wxT("miniaudio failure"), wxICON_ERROR);
+        return false;
+    }
+
     return true;
 }
 
+int GameApp::OnExit()
+{
+    ma_engine_uninit(&mAudioEngine);
+    return wxAppBase::OnExit();
+}
